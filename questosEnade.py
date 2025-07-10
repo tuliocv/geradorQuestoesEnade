23541d8c065b6cf7bc155c7df7655b2548e1f38d
tuliocv <130805106+tuliocv@users.noreply.github.com>
2025-07-10 10:20:31 -0300

    Update questosEnade.py

--- questosEnade.py
+++ questosEnade.py
@@ -320,7 +320,7 @@ TEXTO-BASE:
 {st.session_state.text_base}
 {referencia_texto}
 Por favor, siga EXATAMENTE o formato acima e não altere o texto-base. Uso o texto-base na forma originnal. 
-Não incluir as palavras NA SAÍDA: CONTEXTUALIZAÇÃO, ENUNCIADO e TEXTO-BASE.
+Não incluir as palavras NA SAÍDA: CONTEXTUALIZAÇÃO e TEXTO-BASE.
 """
             out = chamar_llm(
                 [{"role": "system", "content": sys_p},
